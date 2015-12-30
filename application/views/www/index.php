d72cb9a37edf2a437eed0636060dec65b2ee4669
panhongsheng <xingyewo188@163.com>
2015-12-30 14:39:22 +0800

    [增加]增加文件

---
+++ application/views/www/index.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+<div id="container">
+	赋值变量：<?php echo $content;?><br>
+	赋值数组：<?php print_r($array_list);?><br>
+</body>
+</html>
